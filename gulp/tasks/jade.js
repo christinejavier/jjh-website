72867cff989539bc7eddb202cf05e203c1a39270
Michelle Huang <mhuangsmailbox@gmail.com>
2016-09-01 00:52:19 -0700

    adding json data wip

--- gulp/tasks/jade.js
+++ gulp/tasks/jade.js
@@ -10,6 +10,23 @@ var JADE_PATH = "src/jade/**/*.jade";
 gulp.task("jade", function() {
 	console.log("starting jade task");
 	return gulp.src(JADE_PATH)
+		// this doesn't work https://www.npmjs.com/package/gulp-data
+		.pipe(data(function(file){
+			return require("src/jade/api/" + path.basename(file.path, "listings.jade") + ".json");
+		}))
+
+		//doesn't work
+		// .pipe(data(function(){
+		// 	return require("./src/jade/api/listings.jade.json")
+		// }))
+		
+		//doesn't work. fs no longer a plugin?? https://codepen.io/hoichi/post/json-to-jade-in-gulp 
+		// .pipe(data(function(file) {
+		// 	return JSON.parse(
+		//     	fs.readFileSync('./src/jade/api/listings.jade.json')
+		// 	);
+		// }))
+		
 		.pipe(plumber(function(err){
 			console.log("styles task error");
 			console.log(err);
