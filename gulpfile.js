978b4bac0c3d22ecd04c81164a8edeffd1818243
Michelle Huang <mhuangsmailbox@gmail.com>
2016-09-13 23:37:09 -0700

    rename jade -> pug

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,9 @@ var SCRIPTS_PATH = "src/js/**/*.js",
 	CSS_PATH = "src/css/**/*.css",
 	SASS_PATH = "src/css/**/*.sass",
 	STYLES_PATH = "src/css/**/*.{css,sass}",
-	JADE_PATH = "src/jade/**/*.jade",
-	API_PATH = "src/jade/api/*.json";
+	// JADE_PATH = "src/jade/**/*.jade",
+	PUG_PATH = "./src/pug/**/*.pug",
+	API_PATH = "src/pug/api/*.json";
 
 // pulling in all tasks from the tasks folder
 requireDir("gulp/tasks", {
@@ -24,24 +25,25 @@ gulp.task("watch", function(){
 	gulp.watch(SCRIPTS_PATH, ["js"]);
 	// gulp.watch(STYLES_PATH, ["styles"]);
 	gulp.watch([CSS_PATH, SASS_PATH], ["styles"]);
-	gulp.watch(JADE_PATH, ["jade"]);
+	// gulp.watch(JADE_PATH, ["jade"]);
+	gulp.watch(PUG_PATH, ["pug"]);
 	gulp.watch(API_PATH, ["api"]);
 });
 
 // watch and server
 gulp.task("default", function(callback){
 	//finish styles task before all others async
-	runSequence("styles", ["jade", "api", "js", "sync", "watch"], callback)
+	runSequence("styles", ["pug", "api", "js", "sync", "watch"], callback)
 });
 
 // updates all files, dev build
 gulp.task("build", function(callback){
 	//finish rm task before all others async
-	runSequence("rm", "styles", ["jade", "api", "js", "images", "fav"], callback)
+	runSequence("rm", "styles", ["pug", "api", "js", "images", "fav"], callback)
 });
 
 //run for production version
 gulp.task("build:prod", function(callback){
 	//finish rm task before all others async
-	runSequence("rm", "styles", ["jade:prod", "api", "js", "images", "fav"], callback)
+	runSequence("rm", "styles", ["pug:prod", "api", "js", "images", "fav"], callback)
 });
