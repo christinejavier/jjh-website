2d6e2fa94c0878cc64d8883b2afdaf922aa3e469
Michelle Huang <mhuangsmailbox@gmail.com>
2016-09-19 10:48:43 -0700

    fix accurate listing id on click

--- src/js/script.js
+++ src/js/script.js
@@ -11,15 +11,31 @@ $("#lazyjson").lazyjson({
 });
 
 $.getJSON("/api/listings.json", function(listings){
+	$("#lazyjson").on("mouseover", "a", function(e){
+		var listingInfo = listings.length-1-($(this).data("id"));
+		var address = listings[listingInfo].address1;
+		// window.location.href = "/listings?address="+address;
+		$(this).attr("href", ("/listings/current?address="+encodeURIComponent(address)));
+	});
+
+	/*
 	$("#lazyjson").on("click", "a", function(e){
 		//prevent from linking to other page
 		e.preventDefault(); 
-		var listingInfo = ($(this).data("id"));
-		console.log(listings[listingInfo]);
-	})
-})
+		//store id value, need to reverse id's
+		var listingInfo = listings.length-1-($(this).data("id"));
 
+		// var address = decodeURIComponent(window.location.query.address1);
+		// var listing;
+		
+		var address = listings[listingInfo].address1; //might need to decode this?
 
+		// window.location.href = "/listings?address="+address;
+		window.location.href = "/listings?address="+encodeURIComponent(address);
+
+	});
+	*/
+});
 
 
 
